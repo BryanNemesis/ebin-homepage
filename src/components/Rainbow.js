b96f3a9a78c31118a661aa7ad0169fbbab06413a
Igor Binkowski <ibinkowski@apptension.com>
2022-05-20 14:57:31 +0200

    better rainbow

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -2,15 +2,20 @@ const Rainbow = ({ scrollProgress }) => {
 
   const calculateBarPosition = (scrollProgress) => {
     return {
-      transform: `translate(${-200 + scrollProgress * 140}vw)`
+      transform: `translate(${-150 + scrollProgress * 150}vw)`
     }
   }
 
   return (
     <div className="rainbow-container">
-      <div className="rainbow-bar-1" style={calculateBarPosition(scrollProgress * 1.4)}></div>
-      <div className="rainbow-bar-2" style={calculateBarPosition(scrollProgress * 1.2)}></div>
-      <div className="rainbow-bar-3" style={calculateBarPosition(scrollProgress)}></div>
+      <div className="rainbow-bars">
+        <div className="rainbow-bar-1" style={calculateBarPosition(scrollProgress * 1.2)}></div>
+        <div className="rainbow-bar-2" style={calculateBarPosition(scrollProgress * 1.15)}></div>
+        <div className="rainbow-bar-3" style={calculateBarPosition(scrollProgress * 1.1)}></div>
+        <div className="rainbow-bar-4" style={calculateBarPosition(scrollProgress * 1.05)}></div>
+        <div className="rainbow-bar-5" style={calculateBarPosition(scrollProgress )}></div>
+        <div className="rainbow-bar-6" style={calculateBarPosition(scrollProgress * 0.95)}></div>
+      </div>
     </div>
   )
 }
